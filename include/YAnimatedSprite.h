4dec47ca68ff7286cc009b49c8d57e7c13384395
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-16 12:35:46 -0300

    Change nextFrame method access from YAnimatedSprite class

--- include/YAnimatedSprite.h
+++ include/YAnimatedSprite.h
@@ -8,29 +8,85 @@
 /** SDL2 **/
 #include "SDL.h"
 
-/** YDA **/
-#include "YPoint.h"
-
 class YAnimatedSprite : public YSprite
 {
 public:
+	//! Default constructor
+	YAnimatedSprite(): YSprite(NULL),
+					   m_frame(YFrame(0, 0, 0)),
+					   m_frameWidth(0),
+					   m_frameHeight(0),
+					   m_fps(0),
+                       m_paused(false){};
+	//! Constructor with arguments
+	/*
+	  \param Texture
+	  \param Settings used to animate the game object
+	  \param Frame width
+	  \param Frame Height
+	  \param Animation FPS (Frame Per Second)
+	*/
 	YAnimatedSprite(SDL_Texture* a_texture,
-					YPoint a_point,
-					YFrame a_frame);
-	SDL_Rect nextFrame();
-	SDL_Rect rect(float a_size = 1.0);
+					YFrame a_frame,
+					int a_frameWidth,
+					int a_frameHeight,
+					int a_framesPerSecond):
+					YSprite(a_texture),
+					m_frame(a_frame),
+					m_frameWidth(a_frameWidth),
+					m_frameHeight(a_frameHeight),
+					m_fps(a_framesPerSecond)
+	{
+		m_frame = a_frame;
+		m_cols = (float)m_width / (float)m_frameWidth;
+		m_rows = (float)m_height / (float)m_frameHeight;
+		m_previousTick = SDL_GetTicks();
+        m_updateCycle = 1000 / m_fps;
+        m_paused = false;
+	};
+    //! Rect method overriding
+    SDL_Rect rect();
+	//! Pause the sprite animation
+	/*!
+	  \param Pause or continue animation
+	*/
 	void pause(bool a_value);
+	//! Setting the current animation frame
+	/*!
+	  \param Frame index
+	*/
 	void currentFrame(int a_frame);
-	void firstFrame(int a_frame);
-	void lastFrame(int a_frame);
+	//! Setting the first and the last frame animation index
+	/*!
+	  \param First frame index
+	  \param Last frame index
+	*/
+	void frameRange(int a_first,
+					int a_last);
+	/*!
+	  \return Frame width
+	*/
+    int frameWidth();
+    /*!
+      \return Fram Height
+    */
+    int frameHeight();
 protected:
-	YFrame m_frame;
+	YFrame m_frame; //! Frame settings
+	int m_frameWidth; //! Frame width
+	int m_frameHeight; //! Frame height
+	int m_fps; //! Animation speed (Frames per Second)
 private:
 	bool m_paused;
 	int m_rows;
 	int m_cols;
 	Uint32 m_updateCycle;
 	Uint32 m_previousTick;
+    //! Next rect of the animation frame
+    /*!
+     \return rect of the next frame
+     */
+    SDL_Rect nextFrame();    
 };
 
 #endif /** __YANIMATED_SPRITE__ **/
--- src/YAnimatedSprite.cpp
+++ src/YAnimatedSprite.cpp
@@ -1,61 +1,9 @@
 #include "YAnimatedSprite.h"
 
-YAnimatedSprite::YAnimatedSprite(SDL_Texture* a_texture,
-								 YPoint a_point,
-								 YFrame a_frame): 
-								 YSprite(a_texture,
-										 a_point)
-									
+/** Public Methods **/
+SDL_Rect YAnimatedSprite::rect()
 {
-	m_frame = a_frame;
-	m_cols = (float)m_width / (float)m_frame.width;
-	m_rows = (float)m_height / (float)m_frame.height;
-	m_previousTick = SDL_GetTicks();
-	m_updateCycle = 1000 / a_frame.fps;
-}
-
-SDL_Rect YAnimatedSprite::nextFrame()
-{
-	SDL_Rect result = {};
-
-	result.x = (m_frame.current % m_cols) * m_frame.width;
-	result.y = (m_frame.current / m_cols) * m_frame.height;
-	result.h = m_frame.height;
-	result.w = m_frame.width;
-
-	if (!m_paused)
-	{
-		if (SDL_GetTicks() - m_previousTick > m_updateCycle)
-		{
-			m_previousTick = SDL_GetTicks();
-
-			if (m_frame.current + 1 <= m_frame.last)
-			{
-				++m_frame.current;
-			}
-			else
-			{
-				m_frame.current = m_frame.first;
-			}
-		}
-	}
-
-	return result;
-}
-
-SDL_Rect YAnimatedSprite::rect(float a_size)
-{
-	SDL_Rect result = {};
-
-	if (m_visible == true)
-	{
-		result.x = m_position.x();
-		result.y = m_position.y();
-		result.w = m_frame.width * a_size;
-		result.h = m_frame.height * a_size;
-	}
-
-	return result;
+    return nextFrame();
 }
 
 void YAnimatedSprite::pause(bool a_value)
@@ -68,12 +16,48 @@ void YAnimatedSprite::currentFrame(int a_frame)
 	m_frame.current = a_frame;
 }
 
-void YAnimatedSprite::firstFrame(int a_frame)
+void YAnimatedSprite::frameRange(int a_first, int a_last)
 {
-	m_frame.first = a_frame;
+    m_frame.first = a_first;
+    m_frame.last = a_last;
 }
 
-void YAnimatedSprite::lastFrame(int a_frame)
+int YAnimatedSprite::frameWidth()
 {
-	m_frame.last = a_frame;
+    return m_frameWidth;
+}
+
+int YAnimatedSprite::frameHeight()
+{
+    return m_frameHeight;
+}
+
+/** Private Methods **/
+SDL_Rect YAnimatedSprite::nextFrame()
+{
+    SDL_Rect result = {};
+    
+    result.x = (m_frame.current % m_cols) * m_frameWidth;
+    result.y = (m_frame.current / m_cols) * m_frameHeight;
+    result.h = m_frameHeight;
+    result.w = m_frameWidth;
+    
+    if (!m_paused)
+    {
+        if (SDL_GetTicks() - m_previousTick > m_updateCycle)
+        {
+            m_previousTick = SDL_GetTicks();
+            
+            if (m_frame.current + 1 <= m_frame.last)
+            {
+                ++m_frame.current;
+            }
+            else
+            {
+                m_frame.current = m_frame.first;
+            }
+        }
+    }
+    
+    return result;
 }
