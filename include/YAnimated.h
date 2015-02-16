fbff55718ba32fc5f63c71f4251a679093ec2a44
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-16 12:40:34 -0300

    Remove width, height and fps variable from YFrame struct

--- include/YAnimated.h
+++ include/YAnimated.h
@@ -1,27 +1,30 @@
 #ifndef __YANIMATED__
 #define __YANIMATED__
 
+/*!
+  Store animation frame indexes
+*/
 typedef struct YFrame
 {
-	int current;
-	int first;
-	int last;
-	int width;
-	int height;
-	int fps;
-	YFrame(){};
+	//! Default Constructor
+	YFrame(): current(0),
+			  first(0),
+			  last(0){};
+	//! Constructor with parameters
+	/*!
+	  \param Current animation frame index
+	  \param First animation frame index
+	  \param Last animation frame index
+	*/
 	YFrame(int a_current,
 		   int a_first,
-		   int a_last,
-		   int a_width,
-		   int a_height,
-		   int a_fps):
+		   int a_last):
 		   current(a_current),
 		   first(a_first),
-		   last(a_last),
-		   width(a_width),
-		   height(a_height),
-		   fps(a_fps){};
+		   last(a_last){};
+	int current; //! Current frame index
+	int first; //! First frame index
+	int last; //! Last frame index		   
 } YFrame;
 
 #endif /** __YANIMATED__ **/
