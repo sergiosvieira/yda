d5638f8ae52ebf9320608a72c311aec593205fc7
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-16 12:30:54 -0300

    Adds a z component on YPoint class

--- include/YPoint.h
+++ include/YPoint.h
@@ -1,8 +1,8 @@
 #ifndef __YPOINT__
 #define __YPOINT__
 
-/** YDA **/
-#include "YVector.h"
+/** Forwarding class **/
+class YVector;
 
 /*!
   A point is a abstract concept used in games to represents a game object positioning
@@ -10,15 +10,23 @@
 class YPoint
 {
 public:
-	//!	Constructor with arguments
+	//!	Constructor with two arguments
 	YPoint(float a_x,
 		   float a_y):
 		   m_x(a_x),
-		   m_y(a_y){};
-	//! Constructor without arguments
-	YPoint():
-		   m_x(0.f),
-		   m_y(0.f){};
+		   m_y(a_y),
+           m_z(0.f){};
+    //! Constructor with three arguments
+    YPoint(float a_x,
+           float a_y,
+           float a_z):
+           m_x(a_x),
+           m_y(a_y),
+           m_z(a_z){};
+    //! Constructor without arguments
+	YPoint(): m_x(0.f),
+              m_y(0.f),
+              m_z(0.f){};
 	//! Point component
 	/*!
 	  \return x component of the point
@@ -29,6 +37,11 @@ public:
 	  \return y component of the point
 	*/
 	float y() const;
+    //! Point component
+    /*!
+     \return z component of the point
+     */
+    float z() const;
 	//! Creates a new point added to a vector using operator overriding
 	/*!
 	  \return a new YPoint
@@ -37,6 +50,7 @@ public:
 protected:
 	float m_x; //! x component of the point
 	float m_y; //! y component of the pont
+    float m_z; //! z component of the point
 };
 
 #endif /** __YPOINT__ **/
--- src/YPoint.cpp
+++ src/YPoint.cpp
@@ -1,8 +1,14 @@
 #include "YPoint.h"
 
+/** YDA **/
+#include "YVector.h"
+
+
 YPoint YPoint::add(YVector& a_vector) const
 {
-	return YPoint(m_x + a_vector.x(), m_y + a_vector.y());
+	return YPoint(m_x + a_vector.x(),
+                  m_y + a_vector.y(),
+                  m_z + a_vector.z());
 }
 
 float YPoint::x() const
@@ -13,4 +19,9 @@ float YPoint::x() const
 float YPoint::y() const
 {
 	return m_y;
+}
+
+float YPoint::z() const
+{
+    return m_z;
 }
