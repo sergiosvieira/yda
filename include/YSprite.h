db3b628556a5c5d52170b6e745c9257587f3641d
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-16 12:33:42 -0300

    Remove position, width and height variables of the YSprite class

--- include/YSprite.h
+++ include/YSprite.h
@@ -4,9 +4,9 @@
 /** C++ **/
 #include <string>
 
-/** SDL2 **/
-#include <SDL.h>
-#include <SDL_image.h>
+/** Forwarding class **/
+struct SDL_Texture;
+struct SDL_Rect;
 
 /** YDA **/
 #include "YPoint.h"
@@ -14,22 +14,15 @@
 class YSprite
 {
 public:
-	YSprite(SDL_Texture* a_texture,
-			YPoint a_position);
+	YSprite(SDL_Texture* a_texture);
 	SDL_Texture* texture();
-	YPoint position();
-	void position(YPoint a_position);
-	void visible(bool a_value);
-	bool isVisible();
 	int width();
 	int height();
 	SDL_Rect rect();
 protected:
-	YPoint m_position;
 	int m_width;
 	int m_height;
 	SDL_Texture* m_texture;
-	bool m_visible;
 };
 
 #endif /** __YSPRITE __ **/
--- src/YSprite.cpp
+++ src/YSprite.cpp
@@ -3,12 +3,14 @@
 /** C++ **/
 #include <cassert>
 
+/** SDL2 **/
+#include <SDL.h>
+#include <SDL_image.h>
 
-YSprite::YSprite(SDL_Texture* a_texture,
-				 YPoint a_point)
+
+YSprite::YSprite(SDL_Texture* a_texture)
 {
 	m_texture = a_texture;
-	m_position = a_point;
 
 	/** getting texture width and height **/
 	SDL_QueryTexture(a_texture, 
@@ -16,32 +18,11 @@ YSprite::YSprite(SDL_Texture* a_texture,
 					 NULL, 
 					 &m_width, 
 					 &m_height);
-	m_visible = true;
 }
 
 SDL_Texture* YSprite::texture()
 {
 	return m_texture;
-}
-
-YPoint YSprite::position()
-{
-	return m_position;
-}
-
-void YSprite::position(YPoint a_point)
-{
-	m_position = a_point;
-}
-
-void YSprite::visible(bool a_value)
-{
-	m_visible = a_value;
-}
-
-bool YSprite::isVisible()
-{
-	return m_visible;
 }
 
 int YSprite::width()
@@ -56,15 +37,10 @@ int YSprite::height()
 
 SDL_Rect YSprite::rect()
 {
-	SDL_Rect result = {};
+	SDL_Rect result = {}; // Fill SDL_Rect with zero value
 
-	if (m_visible == true)
-	{
-		result.x = m_position.x();
-		result.y = m_position.y();
-		result.w = m_width;
-		result.h = m_height;
-	}
+	result.w = m_width;
+	result.h = m_height;
 
 	return result;
 }
