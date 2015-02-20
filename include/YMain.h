f3996a96a9e6831bd8000b8769bd941393f12814
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-19 22:04:33 -0300

    Fix indent

--- include/YMain.h
+++ include/YMain.h
@@ -10,27 +10,27 @@
 class YMain
 {
 public:
-	typedef enum
-	{
-		NONE,
-		CREATING_ERROR,
-		RENDERING_ERROR
-	} Error;
-	typedef std::function<void(SDL_Event*, float)> FunctionUpdate;
-	typedef std::function<void(SDL_Renderer*)> FunctionRender;
-	YMain(const char* a_title,
-		  int a_width,
-		  int a_height,
-		  Error* a_error = nullptr);
-	~YMain();
-	void start(FunctionUpdate* a_update,
+    typedef enum
+    {
+        NONE,
+        CREATING_ERROR,
+        RENDERING_ERROR
+    } Error;
+    typedef std::function<void(SDL_Event*, float)> FunctionUpdate;
+    typedef std::function<void(SDL_Renderer*)> FunctionRender;
+    YMain(const char* a_title,
+          int a_width,
+          int a_height,
+          Error* a_error = nullptr);
+    ~YMain();
+    void start(FunctionUpdate* a_update,
                FunctionRender* a_render,
                int a_maxFrameRate,
                int a_skipFrames);
-	SDL_Renderer* renderer();
+    SDL_Renderer* renderer();
 protected:
-	SDL_Window* m_window;
-	SDL_Renderer* m_renderer;
+    SDL_Window* m_window;
+    SDL_Renderer* m_renderer;
 private:
 };
 
