b00cedc4d5c99e8024e40c31ab5b4e444dc4474a
Sergio Vieira <sergiosvieira@gmail.com>
2015-02-19 21:53:15 -0300

    Update main.cpp from Z-Order sample considering YObjectManager

--- samples/z-order/main.cpp
+++ samples/z-order/main.cpp
@@ -5,6 +5,7 @@
 #include "YSprite.h"
 #include "YSpriteSheet.h"
 #include "YObject.h"
+#include "YObjectManager.h"
 #include "Bird.h"
 #include "Cloud.h"
 
@@ -12,45 +13,66 @@
 #include <array>
 #include <algorithm>
 
-int main(int argc, char** argv)
+const char* kCloud01 = "cloud_01";
+const char* kCloud02 = "cloud_02";
+const char* kRedBird = "red_bird";
+
+void createClouds(std::vector<Cloud*>&& a_clouds,
+                  int a_size,
+                  YSpriteManager* a_manager)
 {
-    struct
+    a_clouds.reserve(a_size);
+    
+    for (int i = 0; i < a_size; ++i)
     {
-        bool operator()(YObject* a_first, YObject* a_second) const
-        {
-            return a_first->position().z() < a_second->position().z();
-        }
-    } Comparator;
-    
-    const char* kCloud01 = "cloud01";
-    const char* kCloud02 = "cloud02";
-    const int size = 20;
-    std::array<YObject*, size> objects;
-    YMain* game = new YMain("Z-Order - sergiosvieira@gmail.com",
-                            640,
-                            480);
-    YSpriteManager* manager = new YSpriteManager(game->renderer());
-    
-    manager->add(kCloud01, YFileSystem::fullPathName({"gfx"},
-                                                     "cloud_01.png"));
-    manager->add(kCloud02, YFileSystem::fullPathName({"gfx"},
-                                                     "cloud_02.png"));
-    manager->add("red_bird", YFileSystem::fullPathName({"gfx"},
-                                                    "red_bird_set.png"));
-    
-    for (int i = 0; i < size - 1; ++i)
-    {
-        const char* name = (i % 2 == 0) ? kCloud02 : kCloud02;
+        const char* name = (i % 2 == 0) ? kCloud01 : kCloud02;
         float z = (i % 2 == 0) ? 0.f : 2.f;
-        YSprite* cloudSprite = new YSprite(manager->findByName(name));
+        YSprite* cloudSprite = new YSprite(a_manager->findByName(name));
         Cloud* cloud01 = new Cloud(cloudSprite);
         YPoint position = cloud01->position();
         
         position.z(z);
         cloud01->position(position);
-        objects[i] = cloud01;
+        a_clouds.push_back(cloud01);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    typedef struct
+    {
+        std::string key;
+        std::string value;
+    } KeyValue;
+    
+    YMain* game = new YMain("Z-Order - sergiosvieira@gmail.com", 640, 480);
+    YObjectManager* objectManager = new YObjectManager();
+    
+    /** Load Resources **/
+    YSpriteManager* manager = new YSpriteManager(game->renderer());
+    std::array<KeyValue, 3> images = {
+      {
+          {kCloud01, "cloud_01.png"},
+          {kCloud02, "cloud_02.png"},
+          {kRedBird, "red_bird_set.png"}
+      }
+    };
+    
+    for (const KeyValue& kv: images)
+    {
+        manager->add(kv.key.c_str(),
+                     YFileSystem::fullPathName({"gfx"},
+                                               kv.value.c_str()));
     }
     
+    /** Creates Clouds **/
+    std::vector<Cloud*> clouds;
+    createClouds(std::move(clouds),
+                 100,
+                 manager);
+    
+
+    /** Creates Bird **/
     YSpriteSheet* birdSprite = new YSpriteSheet(manager->findByName("red_bird"),
                                                 YFrame(0, 0, 5),
                                                 64,
@@ -58,41 +80,19 @@ int main(int argc, char** argv)
                                                 20);
     Bird* bird = new Bird(birdSprite);
     
-    objects[size - 1] = bird;
-    
-    YMain::FunctionUpdate update = [&](SDL_Event* a_event, float a_deltaT)
+    /** Update/Render Objects **/
+    for (Cloud* cloud: clouds)
     {
-        std::sort(objects.begin(),
-                  objects.end(),
-                  Comparator);
-        
-        for (int i = 0; i < size; ++i)
-        {
-            objects[i]->processEvents(a_event);
-            objects[i]->update(a_deltaT);
-        }
-    };
+        objectManager->add(cloud);
+    }
     
-    YMain::FunctionRender renderer = [&](SDL_Renderer* a_renderer)
-    {
-        for (int i = 0; i < size; ++i)
-        {
-            objects[i]->render(a_renderer);
-        }
-    };
+    objectManager->add(bird);
+//    objectManager->updateRender();
     
-    game->start(&update,
-                &renderer,
+    game->start(objectManager->updater(),
+                objectManager->renderer(),
                 25,
                 5);
-    
-    for (int i = 0; i < size; ++i)
-    {
-        if (objects[i] != nullptr)
-        {
-            delete objects[i];
-        }
-    }
     
     return 0;
 }
